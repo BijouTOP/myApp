261cfb2dfcdd503ea85db249d4e0c0d29a3b6bc4
BijouTOP <guilhermeroquesousa@hotmail.com>
2025-01-12 06:30:06 +0000

    progress...

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
+  standalone : false,
 })
 export class Tab1Page {
+  items: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private loadingController: LoadingController
+  ) {}
 
-  sendRequest() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
+  async sendRequest() {
+    const url = 'https://mobile-api-one.vercel.app/api/travels';
 
-    this.http.get(url).subscribe((response) => {
-      console.log(response);
+    const username = 'sousaguilherme@ipvc.pt';
+    const password = '%2eV!Esu';
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Basic ' + btoa(username + ':' + password)
     });
 
-}}
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+
+    await loading.present();
+
+    this.http.get<any[]>(url, { headers }).subscribe({
+      next: (response) => {
+        this.items = response;
+        console.log(response);
+        loading.dismiss();
+      },
+      error: (error) => {
+        console.error('Error:', error);
+        loading.dismiss();
+      }
+    });
+  }
+}
